594ee780c4ab81872ebe39e6d06a0ac0becb71b0
Marco Boschma <marcoboschma@live.nl>
2022-09-21 16:11:35 +0200

    Basic controctor is done, doing research about rotating the Vector3

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/Vector3.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/Vector3.java
@@ -9,15 +9,31 @@ public class Vector3 {
 	private SceneObject sceneObject;
 
 	public Vector3(float x, float y, float z){
-
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 
+	//*** Get the current xyz
 	public Vector3 GetVector3() {
-		return null;
+		return new Vector3(x,y,z);
 	}
 
 	public void SetVector(float x, float y, float z) {
-
 	}
+
+	//temp rotation calculations
+	public Vector3 RotateVector3(Vector3 vector, Vector3 axis, float degrees){
+
+		return new Vector3(0,0,0);
+	}
+	public Vector3 RotateUpByDegrees(Vector3 vector, int degrees){
+
+		//max rotation is 360
+
+
+		return new Vector3(0,0,0);
+	}
+
 
 }
