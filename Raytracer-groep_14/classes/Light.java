2b741a9c537c7605e889cff32cdc9d5311b1d572
LucavanderHeide <l.vanderheideprive@gmail.com>
2022-09-21 21:02:20 +0200

    Update light.java

---
+++ Raytracer-groep_14/classes/Light.java
@@ -0,0 +1,16 @@
+package org.raytracer.classes;
+
+public class Light {
+
+    private Color colour;
+
+    private Vector3 position;
+
+    private float Intecity;
+
+    public void SetIntencity(int Brightness){
+
+    }
+
+
+}
