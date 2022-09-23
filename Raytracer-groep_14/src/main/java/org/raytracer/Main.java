21e527be3ac66e5b39deb628aa79604ed88e4194
Marco Boschma <marcoboschma@live.nl>
2022-09-23 14:32:03 +0200

    Initilize scene manager in the main and use it to create a new scene

--- Raytracer-groep_14/src/main/java/org/raytracer/Main.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/Main.java
@@ -1,7 +1,11 @@
 package org.raytracer;
 
+import org.raytracer.classes.SceneManager;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SceneManager manager = new SceneManager();
+        manager.SetupScene();
     }
 }
