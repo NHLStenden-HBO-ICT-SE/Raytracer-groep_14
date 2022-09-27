ff07997eaf6bc87cd6c5f3083d6f00e4a217df29
GreyHunt3r <svenimholz@icloud.com>
2022-09-27 13:29:03 +0200

    Update Camera.java

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/Camera.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/Camera.java
@@ -2,13 +2,15 @@ package org.raytracer.classes;
 
 public class Camera {
     private float fieldOfView;
-    private Vector3 position; //ToDo add position of (0,0,0)
-    private float pitch;
-    private float yaw;
+    private Vector3 position;
+    private Vector3 direction;
     private Vector3 center;
     private Vector3 topLeft, topRight, bottomLeft;
 
     public Camera() {
-        this.position = new Vector3(0,0,0);
+        this.position = new Vector3(0, 0, 0);
+        this.direction = new Vector3(1, 0, 0);
+        this.fieldOfView = 50;
+        this.center =
     }
 }
