af7a7f3f580c17b33a8b2185e1fde29c08a837c3
marcoboschma <39697145+marcoboschma@users.noreply.github.com>
2022-10-03 15:20:15 +0200

    Added a todo, with a check if the frame has already been made, the frame is yet empty but the next step will be the creation of a mockup

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/UICanvas.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/UICanvas.java
@@ -8,11 +8,13 @@ public class UICanvas{
 
     PixelData [][] pixelData;
 
+    Camera cam;
 
     //todo create a camera object
     //todo set the pixels from the screen
     //todo try to save the pixels inside some class containing a multidemensional array
     //todo try to create a few empty raycasts
+    //todo make a mockup pixelloop that will try to call raycasts through the camera, looping with x and y or u and v
 
     public UICanvas(int height, int width){
 
@@ -23,6 +25,7 @@ public class UICanvas{
         }
         canvasFrame.setSize(height, width);
         canvasFrame.setVisible(true);
+        cam = new Camera();
 
     }
     public boolean createNewFrame(){
