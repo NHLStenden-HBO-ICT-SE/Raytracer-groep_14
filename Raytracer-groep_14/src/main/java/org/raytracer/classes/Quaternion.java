6b2013bbe506bc98b92a015a1324ea9e4f44e400
marcoboschma <39697145+marcoboschma@users.noreply.github.com>
2022-10-04 14:17:06 +0200

    Things are getting hard, added more comments

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/Quaternion.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/Quaternion.java
@@ -6,12 +6,25 @@ package org.raytracer.classes;
  */
 public class Quaternion {
 
-    public float x;
-    public float y;
-    public float z;
-    public float w;
+    public float x; //w
+    public float y; //i
+    public float z; //j
+    /**
+     * Fake dimension
+     */
+    public float w; // the amount to rotate over the axis, shouldn't be able to turn more then 180
 
 
+    /*
+        Let Q1 and Q2 be two quaternions, which are defined, respectively, as (w1, x1, y1, z1) and (w2, x2, y2, z2).
+        (Q1 * Q2).w = (w1w2 - x1x2 - y1y2 - z1z2)
+        (Q1 * Q2).x = (w1x2 + x1w2 + y1z2 - z1y2)
+        (Q1 * Q2).y = (w1y2 - x1z2 + y1w2 + z1x2)
+        (Q1 * Q2).z = (w1z2 + x1y2 - y1x2 + z1w2
+     */
+
+    //q = w + xi + yj + zk
+    //w should be rotation
     public Quaternion(){
         this.x = 0;
         this.y = 0;
