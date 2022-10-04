ab0b226fe683d33fedebc219e93aaa8c15619b47
marcoboschma <39697145+marcoboschma@users.noreply.github.com>
2022-10-04 08:47:19 +0200

    Made a castline method that will try to cast multiple rays

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/Raycast.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/Raycast.java
@@ -2,4 +2,26 @@ package org.raytracer.classes;
 
 public class Raycast {
 
+
+    /**
+     * Cast multiple rays and return colours or a default if the ray goes out of range
+     */
+    public void castLine(int rayReach, Camera camera, int width, int height){
+
+        Color pixDotCol = new Color();
+
+        PixelData[][] pixelData = new PixelData[width][height];
+        for (int i = 0; i < width; i++) {
+
+            Ray tempray = new Ray(camera.getPosition(), camera.getDirection());
+
+
+            for (int j = 0; j < height; j++) {
+
+            }
+        }
+
+
+
+    }
 }
