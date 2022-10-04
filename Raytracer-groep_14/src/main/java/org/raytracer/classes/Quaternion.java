5c718512a0aa1da8c13ebfd1c3df5914d95ea4ad
marcoboschma <39697145+marcoboschma@users.noreply.github.com>
2022-10-04 14:10:40 +0200

    Made a Quaternion.normalize method, the code and math is based on those of the Vector3.normilize method. Just with one extra number

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/Quaternion.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/Quaternion.java
@@ -12,15 +12,27 @@ public class Quaternion {
     public float w;
 
 
-    Quaternion(){
+    public Quaternion(){
         this.x = 0;
         this.y = 0;
         this.z = 0;
         this.w = 0;
     }
+    public Quaternion(float x, float y, float z, float w){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+
+    public float length() {
+        return (float) Math.sqrt(x*x+y*y+z*z+w*w);
+    }
 
     public Quaternion normalize(){
 
-        return new Quaternion();
+        float length = length();
+        return new Quaternion(this.x / length, this.y / length, this.z / length, this.w / length);
     }
+
 }
