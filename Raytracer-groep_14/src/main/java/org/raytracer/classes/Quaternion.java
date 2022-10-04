f063115b98a6327894a55960efde1697583bca44
marcoboschma <39697145+marcoboschma@users.noreply.github.com>
2022-10-04 14:04:24 +0200

    Added a magic class called Quaternion. This is going to be rather fun to program.

---
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/Quaternion.java
@@ -0,0 +1,26 @@
+package org.raytracer.classes;
+
+
+/**
+ * just admit you don't understand it too
+ */
+public class Quaternion {
+
+    public float x;
+    public float y;
+    public float z;
+    public float w;
+
+
+    Quaternion(){
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.w = 0;
+    }
+
+    public Quaternion normalize(){
+
+        return new Quaternion();
+    }
+}
