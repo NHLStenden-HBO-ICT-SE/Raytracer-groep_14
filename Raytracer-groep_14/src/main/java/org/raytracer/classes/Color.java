79c9d7b8889772b57e36478b8e763a2501cce45b
LucavanderHeide <l.vanderheideprive@gmail.com>
2022-10-11 14:24:34 +0200

    updated color and light

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/Color.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/Color.java
@@ -1,11 +1,54 @@
 package org.raytracer.classes;
 
+
+
 public class Color {
 
-	private float red;
 
-	private float green;
+	private float red = 1;
 
-	private float blue;
+	private float green = 1;
 
+	private float blue = 1;
+
+	public Color(float red, float green, float blue){//todo waarde 1 toegevoegd aan variable, kan dit?
+
+		if(red > 1)
+		{this.red = 1;}
+		else if(red < 0)
+		{this.red = 0;}
+
+		if(green > 1)
+		{this.green = 1;}
+		else if(green < 0)
+		{this.green= 0;}
+
+		if(blue > 1)
+		{this.blue = 1;}
+		else if(blue < 0)
+		{this.blue = 0;}
+
+
+
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+	}
+
+	public float getRed() {
+		return red;
+	}
+
+	public float getBlue() {
+		return blue;
+	}
+
+	public float getGreen() {
+		return green;
+	}
+
+
+	public Color multiply(float value){
+		return new Color(this.red * value, this.green * value, this.blue * value);
+	}
 }
