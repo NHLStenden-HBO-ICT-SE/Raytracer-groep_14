9a0db7b71c825fc06acb9d85276ffaedc6b0d659
marcoboschma <39697145+marcoboschma@users.noreply.github.com>
2022-10-24 10:28:43 +0200

    Created a ThreadWorker class, that will do the work given by the ThreadManager.
    ThreadManager added a integer to set the count of threads that will be made

--- Raytracer-groep_14/src/main/java/org/raytracer/classes/ThreadManager.java
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/ThreadManager.java
@@ -3,7 +3,22 @@ package org.raytracer.classes;
 /*
 //this class will be used to create and manage threads
  */
-public class ThreadManager {
+public class ThreadManager extends Thread{
+
+
+    public int ThreadCount;
+
+    public ThreadManager(int threadCount){
+        this.ThreadCount = threadCount;
+    }
+
+    public boolean reserveThread(){
+        return false;
+    }
+
+    private ThreadWorker createNewThread(){
+        return null;
+    }
 
 
 }
---
+++ Raytracer-groep_14/src/main/java/org/raytracer/classes/ThreadWorker.java
@@ -0,0 +1,8 @@
+package org.raytracer.classes;
+
+public class ThreadWorker extends Thread{
+
+    public void run(){
+        System.out.println("The thread is active");
+    }
+}
